a9efc38973e09b373f09dd21d6fe92936492ea0d
Muhammad Osama Nusrat <45511078+mnusrat786@users.noreply.github.com>
2025-07-18 00:49:07 +0500

    Time: 0 ms (100%), Space: 12.5 MB (14.32%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.py
+++ 2556-convert-the-temperature/2556-convert-the-temperature.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def convertTemperature(self, celsius):
         kelvin = float(celsius) + 273.15
-        fahrenheit = (float(celsius* 1.80)) + 32
-        ans = [kelvin,fahrenheit]
-        return ans
+        fahrenheit = (float(celsius* 1.80)) + 32.00
+        return [kelvin,fahrenheit]
+        # return ans
 
 
  
