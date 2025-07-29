192e559c159fa5663302a8e58dad7df391d669fc
Muhammad Osama Nusrat <45511078+mnusrat786@users.noreply.github.com>
2025-07-30 00:16:14 +0500

    Time: 4 ms (89.81%), Space: 12.4 MB (51.47%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,10 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if x < 0: return False
-        return str(x) == str(x)[::-1]    
+
+        x = str(x)
+        return x == x[::-1]
+        # if x < 0: return False
+        # return str(x) == str(x)[::-1]    
 
 
 
