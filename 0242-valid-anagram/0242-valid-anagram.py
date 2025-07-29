4039e247c4fd6d65d7f2e78ab0f6509171df52b4
Muhammad Osama Nusrat <45511078+mnusrat786@users.noreply.github.com>
2025-07-30 00:36:37 +0500

    Time: 8 ms (78.76%), Space: 17.8 MB (64.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,6 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        return sorted(s) == sorted(t)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
 
         
