5840f4a5d14c3ad76e78f68a4d46b3f77b5f53fa
Muhammad Osama Nusrat <45511078+mnusrat786@users.noreply.github.com>
2025-12-24 00:06:42 +0500

    Time: 11 ms (30.6%), Space: 20.3 MB (98.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,15 +1,27 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if intervals ==[]:
+        if intervals == []:
             return []
-        result = []
+        result= []
         intervals.sort()
         for interval in intervals:
             if result == [] or result[-1][1] < interval[0]:
                 result.append(interval)
             else:
-                result[-1][1]= max(result[-1][1], interval[1])  
-        return result          
+                result[-1][1] = max(result[-1][1],interval[1])
+        return result                       
+# intervals = [[1,3],[2,6],[8,10],[15,18]]
+# result = [[1,3]] 
+# intervals = [[2,6],[8,10],[15,18]]
+# result[0][1], result[-1][1]
+# result[-1][1] = 3  interval[0] = 2  as 3>2  we will merge not append
+# result = [[1,3]]
+# max(3,6)= 6  result[-1][1] =6
 
-        
+
+
+
+
+
+
         
